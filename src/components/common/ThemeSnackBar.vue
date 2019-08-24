b30f68f8eaadc547620905982fdd4dc083bf70a3
GStarP <171250605@smail.nju.edu.cn>
2019-08-24 23:51:37 +0800

    [feat] 提示框改用 v-snackbar

---
+++ src/components/common/ThemeSnackBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-snackbar
+    v-model="show"
+    :color="color"
+    :timeout="2000"
+    top
+  >
+    <v-icon color="white">{{ icon }}</v-icon>
+    <div>{{ text }}</div>
+    <v-icon @click="show = false">mdi-close-circle</v-icon>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      icon: '', // 提示框图标
+      color: '' // 提示框颜色
+    }
+  },
+  props: {
+    // 是否显示提示框
+    show: {
+      type: Boolean,
+      default: false
+    },
+    // 提示框主题
+    theme: {
+      type: String,
+      default: 'success'
+    },
+    // 提示框文字
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  watch: {
+    theme: function (val) {
+      // 根据主题切换图标和颜色
+      switch (val) {
+        case 'success':
+          this.icon = 'mdi-checkbox-marked-circle'
+          this.color = 'success'
+          break
+        case 'error':
+          this.icon = 'mdi-alert'
+          this.color = 'error'
+      }
+    }
+  }
+}
+</script>
--- src/components/search/CompanyList.vue
+++ src/components/search/CompanyList.vue
@@ -50,42 +50,58 @@
         </div>
       </v-card>
     </v-dialog>
-    <v-alert v-model="submitSuccess" class="submit-msg" type="success" prominent dismissible>
-      编辑成功
-    </v-alert>
+    <theme-snackbar
+      :show="snackbarShow"
+      :theme="snackbarTheme"
+      :text="snackbarText"
+    ></theme-snackbar>
   </v-layout>
 </template>
 
 <script>
 export default {
   name: 'company-list',
+  components: {
+    'theme-snackbar': () => import('../common/ThemeSnackBar')
+  },
   data () {
     return {
       dialogShow: false,
       dialogCompanyIndex: 0,
       submitSuccess: false,
-      companyList: [{
-        id: 1,
-        name: '郝呆科技'
-      }, {
-        id: 2,
-        name: '紫宁娱乐'
-      }, {
-        id: 3,
-        name: '小草影业'
-      }, {
-        id: 4,
-        name: '紫婷服装'
-      }, {
-        id: 5,
-        name: '小七电竞'
-      }, {
-        id: 6,
-        name: '青橙武术'
-      }, {
-        id: 7,
-        name: '甜甜学会'
-      }]
+      companyList: [
+        {
+          id: 1,
+          name: '郝呆科技'
+        },
+        {
+          id: 2,
+          name: '紫宁娱乐'
+        },
+        {
+          id: 3,
+          name: '小草影业'
+        },
+        {
+          id: 4,
+          name: '紫婷服装'
+        },
+        {
+          id: 5,
+          name: '小七电竞'
+        },
+        {
+          id: 6,
+          name: '青橙武术'
+        },
+        {
+          id: 7,
+          name: '甜甜学会'
+        }
+      ],
+      snackbarShow: false,
+      snackbarTheme: '',
+      snackbarText: ''
     }
   },
   methods: {
@@ -104,7 +120,18 @@ export default {
     },
     submitEdit () {
       this.dialogShow = false
-      this.submitSuccess = true
+      this.showSuccessSnackBar('修改成功！')
+    },
+    showSuccessSnackBar (text) {
+      this.snackbarTheme = 'success'
+      this.snackbarText = text
+      // 这里会出现一个 BUG, 我故意没有修改掉以作提醒
+      // 当子组件 ThemeSnackBar 中控制显示的值由于 timeout 到时已经变成 false 时
+      // 由于 Vue 强调子组件不允许修改父组件的状态, 所以父组件中控制显示的 snackbarShow 依然为 true
+      // 所以当你再次点击编辑按钮时, 即使没有点击提交, 提示框也会直接弹出
+      // 由此可见将 snackbar 封装组件给父组件调用还是挺难设计的
+      // 目前有一个解决方案就是使用 vuex 统一管理状态, 希望有大神能给出更好地解决方案
+      this.snackbarShow = true
     }
   }
 }
